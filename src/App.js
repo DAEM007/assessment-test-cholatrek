1b762aa778bb55e22f0d9dcc0847419ee1e097b3
EMMANUEL <dammylola299@gmail.com>
2023-02-11 01:49:59 +0100

    Restructured routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 // All styles import
 import './App.css';
+// All components import
+import NavBar from "../src/components/NavBar";
+import SideBar from "../src/components/SideBar";
 // All pages import
 import SignIn from "./pages/signIn/SignIn";
 import Dashboard from "./pages/dashboard/Dashboard";
@@ -10,10 +13,14 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <SignIn />
-        <Routes>
-          <Route path="/dashboard" element={ <Dashboard /> } />
-        </Routes>
+        <SideBar />
+          <div className="container">
+            <NavBar />
+            <Routes>
+              <Route path="/" element={ <Dashboard /> } />
+              <Route path="/signIn" element={ <SignIn /> } />
+            </Routes>
+          </div>
       </Router>
     </div>
   );
