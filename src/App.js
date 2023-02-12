4c7d0c3bb85b7cee041a9258b84c0a16d157ea28
EMMANUEL <dammylola299@gmail.com>
2023-02-12 06:02:15 +0100

    Worked on App component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 // All styles import
 import './App.css';
-// All components import
-import SideBar from "./components/SideBar";
 // All pages import
 import SignIn from "./pages/signIn/SignIn";
 import Dashboard from "./pages/dashboard/Dashboard";
@@ -12,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-          <SideBar />
             <Routes>
               <Route path="/" element={ <Dashboard /> } />
               <Route path="/signIn" element={ <SignIn /> } />
